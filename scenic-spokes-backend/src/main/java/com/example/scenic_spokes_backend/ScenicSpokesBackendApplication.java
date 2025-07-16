2aa78c948badd0bed397586c86eec30bc3e294f7
lsmith1580 <lindseysmith1580@gmail.com>
2025-07-16 00:20:45 -0500

    Added backend project folder

---
+++ scenic-spokes-backend/src/main/java/com/example/scenic_spokes_backend/ScenicSpokesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.scenic_spokes_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScenicSpokesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScenicSpokesBackendApplication.class, args);
+	}
+
+}
---
+++ scenic-spokes-backend/src/test/java/com/example/scenic_spokes_backend/ScenicSpokesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scenic_spokes_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScenicSpokesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
