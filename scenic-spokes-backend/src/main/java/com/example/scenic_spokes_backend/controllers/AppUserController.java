18983732908487cb69d6dd0a0a80732ab9c14fc4
lsmith1580 <lindseysmith1580@gmail.com>
2025-07-23 20:07:09 -0500

    created controller package

---
+++ scenic-spokes-backend/src/main/java/com/example/scenic_spokes_backend/controllers/AppUserController.java
@@ -0,0 +1,11 @@
+package com.example.scenic_spokes_backend.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/appUser")
+public class AppUserController {
+
+
+}
