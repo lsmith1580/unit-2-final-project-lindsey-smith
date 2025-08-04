f557832dfbd560045580d361581c51930c325802
lsmith1580 <lindseysmith1580@gmail.com>
2025-08-04 12:35:50 -0500

    updated appuser repository

--- scenic-spokes-backend/src/main/java/com/example/scenic_spokes_backend/repositories/AppUserRepository.java
+++ scenic-spokes-backend/src/main/java/com/example/scenic_spokes_backend/repositories/AppUserRepository.java
@@ -4,6 +4,9 @@ import com.example.scenic_spokes_backend.entities.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Integer> {
+    Optional<AppUser> findByClerkId(String clerkId); //method to find user by clerk id
 }
