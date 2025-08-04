f3d346b74ad2b4de6e5ba02be9c695a10235a997
lsmith1580 <lindseysmith1580@gmail.com>
2025-08-04 13:21:25 -0500

    created appuserwebhookcontroller

--- scenic-spokes-backend/src/main/java/com/example/scenic_spokes_backend/controllers/AppUserWebhookController.java
+++ scenic-spokes-backend/src/main/java/com/example/scenic_spokes_backend/controllers/AppUserWebhookController.java
@@ -1,5 +1,7 @@
 package com.example.scenic_spokes_backend.controllers;
 
+import com.example.scenic_spokes_backend.dto.ClerkWebhookUserDTO;
+import com.example.scenic_spokes_backend.entities.AppUser;
 import com.example.scenic_spokes_backend.repositories.AppUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -7,8 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/webhooks/clerk")
@@ -18,7 +19,31 @@ public class AppUserWebhookController {
     private final AppUserRepository appUserRepository;
 
     @PostMapping
-    public ResponseEntity<Void> handleClerkWebhook(@RequestBody Map<String, Object> payload) {
-        String
+    //returning void because you don't need to return a body for webhooks
+    public ResponseEntity<Void> handleClerkWebhook(@RequestBody ClerkWebhookUserDTO webhook) {
+        if (!webhook.getType().startsWith("user.")) { //clerk sends events like user.created and user.updated so any time one of those events occur,
+            return ResponseEntity.ok().build();         //it will trigger an update to either create the user or update an existing user
+        }
+
+        ClerkWebhookUserDTO.ClerkUserData data = webhook.getData();
+
+        String clerkId = data.getId();
+        String username = data.getUsername();
+        String firstName = data.getFirst_name();
+        String lastName = data.getLast_name();
+        String email = data.getEmail_addresses() != null && !data.getEmail_addresses().isEmpty()
+                ? data.getEmail_addresses().getFirst().getEmail_address() //since clerk's json stores email address information in an array and I only need to access the email address itself
+                : "";
+        Optional<AppUser> existingAppUser = appUserRepository.findByClerkId(clerkId);
+        AppUser appUser = existingAppUser.orElseGet(AppUser::new); //checks to see if the user already exists, if they do return the existing user, if not return a new user
+
+        appUser.setClerkId(clerkId);
+        appUser.setUsername(username);
+        appUser.setFirstName(firstName);
+        appUser.setLastName(lastName);
+        appUser.setEmail(email);
+
+        appUserRepository.save(appUser);
+        return ResponseEntity.ok().build();
     }
 }
