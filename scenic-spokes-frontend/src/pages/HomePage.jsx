05f99e036276f8e0d998efd307ba2a75b9709347
lsmith1580 <lindseysmith1580@gmail.com>
2025-08-05 05:55:48 -0500

    updated home and about page

--- scenic-spokes-frontend/src/pages/HomePage.jsx
+++ scenic-spokes-frontend/src/pages/HomePage.jsx
@@ -1,16 +1,18 @@
-
 const HomePage = () => {
-    return (
-        <div className="home-page">
-            <h2>Welcome to Scenic Spokes — It's About the Journey</h2>
-            <p>Discover the open road like never before. We’re here for the riders who
-               chase sunsets, mountain curves, and hidden backroads. Plan your
-               next unforgettable journey, check out must-see stops, and connect with
-               a community that knows it’s about the ride, not just the destination.</p>
-            <br/>
-            <p>Start your adventure today. The road is calling.</p>
-        </div>
-    );
+  return (
+    <div className="home-page">
+      <h2>Welcome to Scenic Spokes — It's About the Journey</h2>
+      <p>
+        Discover the open road like never before. We’re here for the riders who
+        chase sunsets, mountain curves, and hidden backroads. Plan your next
+        unforgettable journey, explore upcoming motorcycle events, and connect
+        with a community that knows it’s about the ride, not just the
+        destination.
+      </p>
+      <br />
+      <p>Start your adventure today. The road is calling.</p>
+    </div>
+  );
 };
 
 export default HomePage;
