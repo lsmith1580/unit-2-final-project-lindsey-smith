04bbc35727811302909452f9bd201e9cccb06a02
lsmith1580 <lindseysmith1580@gmail.com>
2025-08-05 04:03:44 -0500

    created configuration class to handle uploaded images

--- scenic-spokes-backend/src/main/java/com/example/scenic_spokes_backend/config/StaticResourceConfig.java
+++ scenic-spokes-backend/src/main/java/com/example/scenic_spokes_backend/config/StaticResourceConfig.java
@@ -1,4 +1,14 @@
 package com.example.scenic_spokes_backend.config;
 
-public class StaticResourceConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticResourceConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry reg) {
+        reg.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/");
+    }
 }
